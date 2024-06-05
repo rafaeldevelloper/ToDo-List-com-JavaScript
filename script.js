737cca64e197f5a58a812473ff4e1c4307d4482f
Rafael Silva <rafael_develloper@hotmail.com>
2024-06-05 13:02:27 -0300

    Add files via upload
    ToDo-List feito com HTML, CSS e JavaScript

---
+++ script.js
@@ -0,0 +1,68 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false,
+  })
+
+  input.value = ''
+
+  mostrarTarefas()
+}
+
+function mostrarTarefas() {
+  let novaLi = ''
+
+  // ['comprar café', 'estudar programação']
+
+  minhaListaDeItens.forEach((item, posicao) => {
+    novaLi =
+      novaLi +
+      `
+
+        <li class="task ${item.concluida && 'done'}">
+            <p>${item.tarefa}</p>
+            <div class="img-task">
+              <img src="./img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})">
+              <img src="./img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
+            </div>
+
+        </li>
+        
+        `
+  })
+
+  listaCompleta.innerHTML = novaLi
+
+  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+}
+
+function concluirTarefa(posicao) {
+  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+  mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+  minhaListaDeItens.splice(posicao, 1)
+
+  mostrarTarefas()
+}
+
+function recarregarTarefas() {
+  const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+  if (tarefasDoLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+  }
+
+  mostrarTarefas()
+}
+
+recarregarTarefas()
+button.addEventListener('click', adicionarNovaTarefa)
